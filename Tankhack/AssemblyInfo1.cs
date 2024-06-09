04c694bd6abae68bafe0bb9dbf93dfc42069168f
joshl <joshl@DESKTOP-T89MRHA>
2024-06-09 12:18:08 -0400

    Add project files.

---
+++ Tankhack/AssemblyInfo1.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using Tankhack; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(Tankhax), "Tank Game Cheat", "0.0.1", "Lurk")]
+[assembly: MelonGame()]
---
+++ Tankhack/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+
+namespace Tankhack
+{
+    public class Tankhax : MelonMod
+    {
+        public Rect windowRect = new Rect(20, 100, 200, 250);
+        public override void OnGUI()
+        {
+            windowRect = GUI.Window(420024, windowRect, MakeGuiWork, "Tank Cheats");
+        }
+
+        public void MakeGuiWork(int windowID)
+        {
+            if (GUILayout.Button("Give Coins"))
+            {
+                GameObject player = GameObject.Find("Player(Clone)");
+                TankPlayer tp = player.GetComponent<TankPlayer>();
+                CoinWallet cw = player.GetComponent<CoinWallet>();
+                cw.SpendCoins(-100000);
+            }
+
+            if (GUILayout.Button("Remove Treads"))
+            {
+                GameObject player = GameObject.Find("Player(Clone)");
+                GameObject treads = player.transform.GetChild(0).gameObject;
+                treads.SetActive(false);
+            }
+        }
+    }
+}
